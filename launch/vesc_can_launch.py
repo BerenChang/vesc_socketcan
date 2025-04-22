06f89967437265aafe09a0c6917e83ccf1838172
BerenChang <berenchang0601@gmail.com>
2025-04-22 10:07:18 +0800

    edit launch file

--- launch/vesc_can_launch.py
+++ launch/vesc_can_launch.py
@@ -30,25 +30,6 @@ def generate_launch_description():
 
 
     return LaunchDescription([
-        Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        )
+        joystick_node,
+        vesc_can_node
     ])
