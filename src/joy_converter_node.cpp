41d1d28ffa48b085f550a1eacee5d5748d0c60df
BerenChang <berenchang0601@gmail.com>
2025-06-06 16:32:38 +0800

    update on mode switching

--- src/joy_converter_node.cpp
+++ src/joy_converter_node.cpp
@@ -6,41 +6,57 @@
 enum ControlMode {
     ThrottleBoard = 0,
     Current = 1,
-    Speed = 2
+    Rpm = 2
 };
 
 class JoyConverterNode : public rclcpp::Node {
 public:
-    JoyConverterNode() : Node("joy_converter_node") {
-        subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    JoyConverterNode() : Node("joy_converter_node"), current_mode_(ThrottleBoard) {
+        using std::placeholders::_1;
+        
+        // Joystick input subscriber
+        input_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10,
             std::bind(&JoyConverterNode::joy_converter_callback, this, std::placeholders::_1));
+        
+        // control mode subscriber 
+        mode_sub_ = this->create_subscription<std_msgs::msg::Int32>("control_mode", rclcpp::QoS(10), std::bind(&JoyConverterNode::mode_callback, this, _1));
             
-        publisher_ = this->create_publisher<vesc_msgs::msg::ThrottleBoard>("/vesc/set_throttle_board", rclcpp::QoS(10));
-    }
+        // three control mode publisher
+        tb_pub_ = this->create_publisher<vesc_msgs::msg::ThrottleBoard>("/vesc/set_throttle_board", rclcpp::QoS(10));
+        current_pub_ = this->create_publisher<vesc_msgs::msg::Current>("/vesc/set_current", rclcpp::QoS(10));
+        rpm_pub_ = this->create_publisher<vesc_msgs::msg::Rpm>("/vesc/set_rpm", rclcpp::QoS(10));
+    } 
         
 private:
+    int current_mode_;   
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr input_sub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr mode_sub_;
+    rclcpp::Publisher<vesc_msgs::msg::ThrottleBoard>::SharedPtr tb_pub_;
+    rclcpp::Publisher<vesc_msgs::msg::Current>::SharedPtr current_pub_;
+    rclcpp::Publisher<vesc_msgs::msg::Rpm>::SharedPtr rpm_pub_;
+    
     void joy_converter_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
-        auto converted_msg = vesc_msgs::msg::ThrottleBoard();
-        converted_msg.throttle = msg->axes[4];
-        converted_msg.board = msg->axes[0]; 
-    
-        // RCLCPP_INFO(this->get_logger(), "Joystick inputs: %f, %f", converted_msg.throttle, converted_msg.board);
-        
-        publisher_->publish(converted_msg);
+        switch (current_mode_) {
+            case ThrottleBoard:
+                auto converted_msg = vesc_msgs::msg::ThrottleBoard();
+                converted_msg.throttle = msg->axes[4];
+                converted_msg.board = msg->axes[0]; 
+                tb_pub_->publish(converted_msg);
+                
+            case Current:
+                auto converted_msg = vesc_msgs::msg::Current();
+                converted_msg.current0 = msg->axes[1];
+                converted_msg.current1 = msg->axes[4];
+                current_pub_->publish(converted_msg);
+                
+            case Rpm:
+                auto converted_msg = vesc_msgs::msg::Rpm();
+                converted_msg.Rpm0 = msg->axes[1];
+                converted_msg.Rpm1 = msg->axes[4];
+                rpm_pub_->publish(converted_msg);
+        }
     }
-    
-    // void joy_converter_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
-    //     auto converted_msg = vesc_msgs::msg::Current();
-    //     converted_msg.current = msg->axes[4];
-        
-        // RCLCPP_INFO(this->get_logger(), "Joystick inputs: %f, %f", converted_msg.throttle, converted_msg.board);
-        
-    //     publisher_->publish(converted_msg);
-    // }
-    
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-    rclcpp::Publisher<vesc_msgs::msg::ThrottleBoard>::SharedPtr publisher_;
 };
 
 int main(int argc, char * argv[]) {
@@ -48,4 +64,4 @@ int main(int argc, char * argv[]) {
     rclcpp::spin(std::make_shared<JoyConverterNode>());
     rclcpp::shutdown();
     return 0;
-}
+}  
